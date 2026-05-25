66abf69a5bc9b56996b4d60c6240cd5b0034f314
Miguel Lima <miguelnmlima07@gmail.com>
2026-05-25 12:20:51 +0100

    Class Deck types certos

--- blackjack/src/main/java/esw/blackjack/Deck.java
+++ blackjack/src/main/java/esw/blackjack/Deck.java
@@ -17,7 +17,7 @@ public class Deck {
     
     private void buildDeck() {
         String[] values = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
-        String[] types = {"C", "D", "H", "S"};
+        String[] types = {"Clubs", "Diamonds", "Hearts", "Spades"};
 
         for (String type : types) {
             for (String value : values) {
