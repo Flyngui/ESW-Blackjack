ee2e08f211c0e0369d4474a098b04fb10898dd47
Miguel Lima <miguelnmlima07@gmail.com>
2026-05-25 12:21:42 +0100

    Class Card path certo

--- blackjack/src/main/java/esw/blackjack/Card.java
+++ blackjack/src/main/java/esw/blackjack/Card.java
@@ -2,34 +2,37 @@ package esw.blackjack;
 
 public class Card {
 
-        String value;
-        String type;
+    String value;
+    String type; 
 
-        Card(String value, String type) {
-            this.value = value;
-            this.type = type;
-        }
-
-        public String toString() {
-            return value + "-" + type;
-        }
-
-        public int getValue() {
-            if ("AJQK".contains(value)) { //A J Q K
-                if (value == "A") {
-                    return 11;
-                }
-                return 10;
-            }
-            return Integer.parseInt(value); //2-10
-        }
-
-        public boolean isAce() {
-            return value == "A";
-        }
-
-        public String getImagePath() {
-            return "./sprites/" + toString() + ".png";
-        }
+    Card(String value, String type) {
+        this.value = value;
+        this.type = type;
     }
 
+    
+    @Override
+    public String toString() {
+        return "card" + type + value;
+    }
+
+    public int getValue() {
+        if ("AJQK".contains(value)) { 
+            if ("A".equals(value)) {
+                return 11;
+            }
+            return 10;
+        }
+        return Integer.parseInt(value); // 2-10
+    }
+
+    public boolean isAce() {
+      
+        return "A".equals(value);
+    }
+
+    public String getImagePath() {
+       
+        return "/sprites/PNG/Cards/" + toString() + ".png";
+    }
+}
