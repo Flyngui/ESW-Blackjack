96002b82976d0b5a0e3e164aed6cdbd1094b6224
guiferre <guilhermemlopes812@gmail.com>
2026-06-05 03:55:01 +0100

    last fixes

--- blackjack/bonus/java/esw/blackjack/Launcher.java
+++
@@ -1,8 +0,0 @@
-package esw.blackjack;
-
-public class Launcher {
-	public static void main(String[] args) {
-		// This bypasses the strict JavaFX module check in fat JARs
-		BlackjackApp.main(args);
-	}
-}
